a49cfafbd4fe41b961191249d168539a0808e6c5
Koki Ibukuro <koki.ibukuro@gmail.com>
2025-02-14 09:56:32 +0100

    Add tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
+import tailwindcss from '@tailwindcss/vite';
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
+
 export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ],
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                404: resolve(__dirname, "404.html")
+                404: resolve(__dirname, "404.html"),
             }
         }
     }
